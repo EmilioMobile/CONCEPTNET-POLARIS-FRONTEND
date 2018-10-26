9f92e4984f5f65f0fb91734587a2b6e3dd421671
Emilio Coronado <emilio.mobile@gmail.com>
2018-10-26 13:24:10 +0900

    2nd commit

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, AfterViewInit, AfterContentInit, DoCheck, OnDestroy, AfterContentChecked, 
+          AfterViewChecked } from '@angular/core';
 import { ChatService, Message } from '../chat.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/scan';
@@ -6,24 +7,68 @@ import 'rxjs/add/operator/scan';
 @Component({
   selector: 'app-chat-dialog',
   templateUrl: './chat-dialog.component.html',
-  styleUrls: ['./chat-dialog.component.css']
+  styleUrls: ['./chat-dialog.component.scss']
 })
-export class ChatDialogComponent implements OnInit {
+export class ChatDialogComponent implements OnInit, AfterViewInit, AfterContentInit,
+                                      OnChanges, DoCheck, OnDestroy, AfterContentChecked, AfterViewChecked {
 
   messages: Observable<Message[]>;
-  formValue: string;
+  formValue = '';
+  container: HTMLElement;
+  DEBUG = false;
 
   constructor(public chat: ChatService) { }
 
   ngOnInit() {
-    // this.chat.talk();
+    this.debug('ngOnInit');
     this.messages = this.chat.conversation.asObservable()
       .scan((acc, val) => acc.concat(val));
   }
 
-  sendMessage() {
-    this.chat.converse(this.formValue);
-    this.formValue = '';
+  ngAfterViewInit() {
+    this.debug('ngAfterViewInit');
   }
 
+  ngAfterContentInit() {
+    this.debug('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewChecked() {
+    this.debug('ngAfterViewChecked');
+    this.updateScroll();
+  }
+
+  ngOnDestroy() {
+    this.debug('ngOnDestroy');
+  }
+
+  ngOnChanges() {
+    this.debug('ngOnChanges');
+  }
+
+  ngDoCheck() {
+    this.debug('ngDoCheck');
+  }
+
+  sendMessage() {
+    if (this.formValue !== '' && this.formValue !== undefined) {
+      this.chat.converse(this.formValue);
+      this.formValue = '';
+    }
+  }
+
+  updateScroll() {
+    this.container = document.getElementById('messages-container');
+    this.container.scrollTop = this.container.scrollHeight;
+  }
+
+  debug(message) {
+    if (this.DEBUG === true) {
+      console.log(message);
+    }
+  }
 }
